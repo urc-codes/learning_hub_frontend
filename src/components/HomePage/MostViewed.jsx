951b37b0636cbcc5c590cb5733481d3059254814
amgenious <henrytweneboah956@gmail.com>
2023-07-08 00:16:51 +0000

    the books apis is working

--- src/components/HomePage/MostViewed.jsx
+++ src/components/HomePage/MostViewed.jsx
@@ -44,10 +44,7 @@ export const MostViewed = () => {
           </button>
           <div className={`panel ${activeIndex === 3 ? "show" : ""}`}>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              
             </p>
           </div>
           <button
--- src/components/HomePage/RecentlyViewed.jsx
+++ src/components/HomePage/RecentlyViewed.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios"
 
 export const RecentlyViewed = () => {
   const [activeIndex, setActiveIndex] = useState();
+
+  const [isLoading, setIsLoading] = useState();
+  const [data, setMobileAppBooks] = useState([]);
 
   const handleAccordionClick = (index) => {
     setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
     panel.style.display === "show";
   };
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/books/mobile_app")
+    .then(response => setMobileAppBooks(response.data))
+    .catch(err => console.log(err))
+       setTimeout(() => {
+            setIsLoading(false);
+          }, 2000); // Set the desired loading time in milliseconds
+    },[])
 
   return (
     <>
@@ -49,12 +62,37 @@ export const RecentlyViewed = () => {
             Mobile App Design
           </button>
           <div className={`panel ${activeIndex === 3 ? "show" : ""}`}>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
+            <div class="featured_book_box">
+              {isLoading ? (
+                <p style={{ color: "black" }}>Loading...</p>
+              ) : (
+                data.map((item) => (
+                  <div class="featured_book_card">
+                    <>
+                      <div class="featurde_book_img">
+                        {/* eslint-disable-next-line */}
+                        <img
+                          src={`https://drive.google.com/uc?id=${item.book_image_id}`}
+                        ></img>
+                      </div>
+                      <div class="featurde_book_tag">
+                        <h3>{item.book_titletitle}</h3>
+                        <p class="writer">{item.author_name}</p>
+                        <p>
+                          <a
+                            target="_blank"
+                            href={item.book_url}
+                            style={{ textDecoration: "none" }}
+                          >
+                            Read Book
+                          </a>
+                        </p>
+                      </div>
+                    </>
+                  </div>
+                ))
+              )}
+            </div>
           </div>
           <button
             className={`accordion ${activeIndex === 4 ? "active" : ""}`}
